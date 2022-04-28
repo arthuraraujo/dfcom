ca9e5b392afacf1572273e245356549a40b3e866
Arthur Araújo <contato@arthuraraujo.com.br>
2022-04-28 07:53:33 -0300

    Removed Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
-import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
-    );
+function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
