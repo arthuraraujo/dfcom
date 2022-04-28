3da59a6d31ee4fe795aab18d1761210f75317c29
Arthur Araújo <contato@arthuraraujo.com.br>
2022-04-28 08:12:05 -0300

    Products

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -7,15 +7,21 @@ export default async function handler(
 ) {
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
-    switch (req.method) {
-        // case "POST":
-        //   let bodyObject = JSON.parse(req.body);
-        //   let newPost = await db.collection("products").insertOne(bodyObject);
-        //   res.json(newPost.ops[0]);
-        //   break;
-        case "GET":
-            const products = await db.collection("products").find({}).toArray();
-            res.json({ status: 200, data: products });
-            break;
+    try {
+        const products = await db.collection("products").find({}).toArray();
+        res.json({ status: 200, data: products });
+    } catch (err: any) {
+        console.log({ err, message: err.message });
     }
+    // switch (req.method) {
+    //     // case "POST":
+    //     //   let bodyObject = JSON.parse(req.body);
+    //     //   let newPost = await db.collection("products").insertOne(bodyObject);
+    //     //   res.json(newPost.ops[0]);
+    //     //   break;
+    //     case "GET":
+    //         const products = await db.collection("products").find({}).toArray();
+    //         res.json({ status: 200, data: products });
+    //         break;
+    // }
 }
